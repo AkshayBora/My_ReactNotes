aaf0285fd0c39be9bdffbb2c9435454ad103adb3
AkshayBora <akshaybora5@gmail.com>
2023-10-09 14:19:20 +0530

    React_Router,Create Routing Configuration,Error Page,useRouteError(custom)_Hook,Children Route,Outlet,Link Component,Single-Page-Application(SPA),Clint-Side and Server-Side routing,Dynamic Routing,Map function,useParams Hook for dynamic id,Clickable card,Link All components

---
+++ Chapter 07 - Finding the Path/src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1>About</h1>
+        <h2>
+          This is Food website where you can order food form different place
+        </h2>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ Chapter 07 - Finding the Path/src/components/Contact.js
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <>
+      <h1>Contact Us Page..!!</h1>
+      <h2>
+        You can contact me on from any of the links which provide on webPage.
+      </h2>
+    </>
+  );
+};
+
+export default Contact;
